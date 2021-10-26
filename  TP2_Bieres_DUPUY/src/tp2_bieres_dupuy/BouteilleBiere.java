ebce7fa38db6d2b6710b4ab8857d8f11d60a5ceb
ROXANA <ROXANA@DESKTOP-V77CGO9>
2021-10-26 20:53:15 +0200

    TP2 Ex1

---
+++  TP2_Bieres_DUPUY/src/tp2_bieres_dupuy/BouteilleBiere.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_dupuy;
+
+/**
+ *
+ * @author ROXANA
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+ 
+ }
+BouteilleBiere uneBiere = new BouteilleBiere() ;
+
+}
