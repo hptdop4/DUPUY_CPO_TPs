2233391fc28adafead1886bf19073d51d8c9dab8
ROXANA <ROXANA@DESKTOP-V77CGO9>
2021-11-06 12:36:20 +0100

    TP2 Ex1 jusqu'à Q12

---  TP2_Bieres_DUPUY/src/tp2_bieres_dupuy/TP2_Bieres_DUPUY.java
+++  TP2_Bieres_DUPUY/src/tp2_bieres_dupuy/TP2_Bieres_DUPUY.java
@@ -14,7 +14,29 @@ public class TP2_Bieres_DUPUY {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    // AVANT LA MISE EN PLACE DU CONSTRUCTEUR
+     /*  BouteilleBiere uneBiere = new BouteilleBiere() ;
+       uneBiere.nom = "Cuvée des trolls";
+       uneBiere.degreAlcool = 7.0 ;
+       uneBiere.brasserie = "Dubuisson";
+       uneBiere.ouverte = false;
+       uneBiere.lireEtiquette();
+       
+       BouteilleBiere deuxiemeBiere = new BouteilleBiere ();
+       deuxiemeBiere.nom = "Leffe";
+       deuxiemeBiere.degreAlcool = 6.6 ;
+       deuxiemeBiere.brasserie = "Abbaye de Leffe";
+       
+       deuxiemeBiere.lireEtiquette();
+    }*/
     
+     //APRES LA MISE EN PLACE DU CONSTRUCTEUR
+     BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
+     uneBiere.lireEtiquette ();
+     BouteilleBiere deuxiemeBiere = new BouteilleBiere ("Desperardos", 5.0, "Mont Blanc");
+     deuxiemeBiere.lireEtiquette ();
+     BouteilleBiere troisiemeBiere = new BouteilleBiere ("Guiness", 6.2, "La Taverne");
+     troisiemeBiere.lireEtiquette ();
+     BouteilleBiere quatriemeBiere = new BouteilleBiere ("Heineken", 7.4, "Le Gapiand");
+     quatriemeBiere.lireEtiquette ();
 }
