1d5ecc411f090ac4c26e9c7c7f8a11c30183f616
ROXANA <ROXANA@DESKTOP-V77CGO9>
2021-11-11 16:37:46 +0100

    TP2 Ex4 dernières questions

---
+++ TP2_relation_1_DUPUY/src/tp2_relation_1_dupuy/Personne.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_dupuy;
+
+/**
+ *
+ * @author ROXANA
+ */
+public class Personne {
+    String nom;
+    String prenom ;
+    int nbVoitures ;
+    Voiture [] liste_voitures ;
+            
+public Personne (String unNom, String unPrenom ) {
+ nom = unNom;
+ prenom = unPrenom;
+ liste_voitures = new Voiture [3] ;
+  }  
+
+    @Override
+    public String toString() {
+    String chaine_a_retourner;
+    chaine_a_retourner = nom + prenom ;
+    return chaine_a_retourner ;
+  }
+}
