e5021cb097eff609346c2eeddbf7dbe8976230f6
ROXANA <ROXANA@DESKTOP-V77CGO9>
2021-11-11 16:37:24 +0100

    TP2 Ex4 dernières questions

---
+++ TP2_relation_1_DUPUY/src/tp2_relation_1_dupuy/Voiture.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_dupuy;
+
+/**
+ *
+ * @author ROXANA
+ */
+public class Voiture {
+    String modele ;
+    String marque ;
+    int puissanceCV ;
+    Personne Proprietaire ;
+    
+    
+public Voiture(String unModele, String uneMarque, int unePuissanceCV, Personne unProprietaire) {
+ modele = unModele;
+ marque = uneMarque;
+ puissanceCV = unePuissanceCV;
+ Proprietaire = unProprietaire;
+ 
+  }        
+
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = modele + marque ;
+return chaine_a_retourner;
+}
+
+}
